f8c5d57f2eecdffd0affcc0aafa79d00b8ec1ee6
Luiza Trindade <luiza.lag@gmail.com>
2021-12-06 23:16:41 -0300

    ajustes entradas de productos con DOM

---
+++ js/shoppingcart.js
@@ -0,0 +1,90 @@
+class Producto {
+    constructor (id, categoria, nombre, color, talle, precio) {
+        this.id = parseFloat(id);
+        this.categoria = categoria.toLowerCase();
+        this.modelo = modelo.toLowerCase();
+        this.color = color.toLowerCase();
+        this.talle = talle.toLowerCase();
+        this.precio = parseFloat(precio);
+        this.stock = 20;
+    }
+
+    precioIVA() {
+        return this.precio * 1.21;
+    }
+
+    precio() {
+        return this.precio;
+    }
+
+    vender () {
+        if (this.stock === 0) {
+            alert ("No hay más stock del producto elegido.");
+        }
+
+        else{
+            console.log ("Producto vendido.")
+            this.stock --;
+        }
+    }
+
+    refill () {
+        this.stock += 20;
+    }
+
+}
+
+const productos = [{ id: 1, categoria: "Bikini", modelo: "vainilla".toUpperCase(), color: "blanco", talle: "S", precio: 2000, imgSrc: "images/fotos/destacado1.jpg"},
+                   { id: 2, categoria: "Bikini", modelo: "sol de toscana".toUpperCase(), color: "amarillo", talle: "M", precio: 2000, imgSrc: "images/fotos/destacado2.jpg"},
+                   { id: 3, categoria: "Bikini", modelo: "mar griego".toUpperCase(), color: "azul", talle: "M", precio: 2000, imgSrc: "images/fotos/destacado3.jpg"},
+                   { id: 4, categoria: "Enteriza", modelo: "caraiva".toUpperCase(), color: "naranja", talle: "L", precio: 2500, imgSrc: "images/fotos/destacado4.jpg"},
+                   { id: 5, categoria: "Enteriza", modelo: "salvador".toUpperCase(), color: "celeste", talle: "S", precio: 2500, imgSrc: "images/fotos/destacado5.jpg"},
+                   { id: 6, categoria: "Enteriza", modelo: "porto seguro".toUpperCase(), color: "violeta", talle: "L", precio: 2500, imgSrc: "images/fotos/destacado6.jpg"},
+                   { id: 7, categoria: "Pareo", modelo: "sandia".toUpperCase(), color: "rojo", talle: "U", precio: 1300, imgSrc: "images/fotos/destacado7.jpg"},
+                   { id: 8, categoria: "Pareo", modelo: "melon".toUpperCase(), color: "verde", talle: "U", precio: 1300, imgSrc: "images/fotos/destacado8.jpg"},
+                   { id: 9, categoria: "Pareo", modelo: "uva".toUpperCase(), color: "violeta", talle: "U", precio: 1300, imgSrc: "images/fotos/destacado9.jpg"}]
+
+// const guardarLocal = (clave, valor) => { localStorage.setItem(clave, valor)};
+
+// for (const producto of productos) {
+//     guardarLocal(producto.id, JSON.stringify(producto))
+// }
+
+// guardarLocal("listaProductos", JSON.stringify(productos));
+
+// const almacenados = JSON.parse (localStorage.getItem("listasProductos"));
+
+let rowNumber = 0;
+let rowContainer = document.getElementById("row-container");
+let templateCard = document.getElementsByClassName("card-template")[0];
+
+for (let i = 0; i < productos.length; i++) {
+    let producto = productos[i];
+    let row = document.getElementById(`row-${rowNumber}`);
+
+    if (i % 3 === 0) {
+        rowNumber++;
+        row = document.createElement("div");
+        row.id = `row-${rowNumber}`;
+        row.className = "col-8 card-group";
+        rowContainer.appendChild(row);
+    } 
+    
+    let newCard = templateCard.cloneNode(true);
+
+    newCard.id = `producto-${producto.id}`;
+    newCard.className = "card";
+    let img = newCard.getElementsByClassName("card-img-top")[0];
+    let title = newCard.getElementsByClassName("card-title")[0];
+    let price = newCard.getElementsByClassName("card-text")[0];
+
+    img.src = producto.imgSrc;
+    title.innerHTML = `${producto.categoria} - ${producto.modelo}`;
+    price.innerHTML = `$ ${producto.precio}`;
+
+    row.appendChild(newCard);
+} 
+
+
+
+
